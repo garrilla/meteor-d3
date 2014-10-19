7ade8a45ce211eeba580fd12c76c1613790c017e
garrilla <garry@garrilla.com>
2014-10-19 01:49:19 +0100

    meteorised 3.4.13 adds test but not published

--- garrilla:d3-tests.js
+++ garrilla:d3-tests.js
@@ -1,5 +1,9 @@
 // Write your tests here!
 // Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
-});
+if(Meteor.isClient) {
+  /** calls the d3.version protype, which returns the version string
+   *  and tests if its the same as the version */
+  Tinytest.add('garrilla:d3 version test', function (test) {
+    test.equal(d3.version, '3.4.13');
+  });
+}
