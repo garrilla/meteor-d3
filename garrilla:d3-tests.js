3c6b85ff98f3a82dfc6065e460f98f45341d3f35
garrilla <garry@garrilla.com>
2014-10-19 01:56:52 +0100

    test notes updated

--- garrilla:d3-tests.js
+++ garrilla:d3-tests.js
@@ -1,8 +1,12 @@
 // Write your tests here!
 // Here is an example.
 if(Meteor.isClient) {
-  /** calls the d3.version protype, which returns the version string
-   *  and tests if its the same as the version */
+  /** calls the d3.version prototype, which returns the version string
+   *  and tests if its the same as the version that is declared here
+   *  TODO change version to string to call from
+   *  this assures that d3 is available, and that it's prototypes
+   *  return values - eveything else we have to assume running based
+   *  d3's own test framework */
   Tinytest.add('garrilla:d3 version test', function (test) {
     test.equal(d3.version, '3.4.13');
   });
